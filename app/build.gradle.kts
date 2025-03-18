203bdb4ac870bc05e9fe08c5fdffaf871bf0641b
EmilianoHinojosa <myemail>
2025-03-17 21:51:27 -0600

    Revert "Device location feature added"
    This reverts commit f558326dcd4ae730b89d8ace37e2ac2dae16185e.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database.ktx)
     implementation(libs.firebase.database)
-    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
